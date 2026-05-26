83a42330c2ba3863529d0080d81f7798704ac268
KAVIN R <rkavin975rk@gmail.com>
2026-05-26 10:46:46 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!= t.length())
+        {
+            return false;
+        }
+        HashMap<Character,Integer> m = new HashMap<>();
+        for(char i : s.toCharArray())
+        {
+            m.put(i,m.getOrDefault(i,0)+1);
+        }
+         for(char i : t.toCharArray())
+        {
+           if(!m.containsKey(i))
+           {
+            return false;
+           }
+           m.put(i,m.get(i)-1);
+           if(m.get(i)==0)
+           {
+            m.remove(i);
+           }
+
+        }
+        return m.isEmpty();
+
+    }
+}
